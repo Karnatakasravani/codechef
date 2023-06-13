584903f2899712b5d408605b161eaa8ed4c686ae
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-06-13 10:22:43 +0530

    Create complete the credits.py

---
+++ complete the credits.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    if n>65:
+        print("Overload")
+    elif n<35:
+        print("Underload")
+    else:
+        print("Normal")
