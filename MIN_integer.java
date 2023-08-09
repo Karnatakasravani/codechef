1379961f5605e5973fe1d8ab3d63a194eeb97590
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-08-09 21:41:49 +0530

    Create MIN_integer.java

---
+++ MIN_integer.java
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def minimumInteger(self, N : int, A : List[int]) -> int:
+        # codeere
+        s=sum(A)
+        c=max(A)
+        if N==1:
+            return A[0]
+        for i in A:
+            if s<=N*i:
+                c=min(c,i)
+        return c
+    
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        A=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.minimumInteger(N, A)
+        
+        print(res)
+        
+
+# } Driver Code Ends
